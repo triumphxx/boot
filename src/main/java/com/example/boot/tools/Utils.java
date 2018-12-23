ef6cb52037305e415b4ce9b1457c312b7ceb8bfc
王玉鹏 <550902023@qq.com>
2018-12-23 20:49:12 +0800

    add metch

--- src/main/java/com/example/boot/tools/Utils.java
+++ src/main/java/com/example/boot/tools/Utils.java
@@ -3,14 +3,21 @@ package com.example.boot.tools;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wang on 2018/12/23.
  */
 @Component
 public class Utils {
+
     public List getList(){
         return new ArrayList();
+    }
+
+    public Map getMap(){
+        return new HashMap();
     }
 }

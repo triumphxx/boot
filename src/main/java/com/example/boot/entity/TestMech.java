8bfd119ef430de37018edd00e17d5272140db39a
wangyp@sinosoft.com <wangyupeng>
2018-12-24 09:12:23 +0800

    '修改构造方法'

--- src/main/java/com/example/boot/entity/TestMech.java
+++ src/main/java/com/example/boot/entity/TestMech.java
@@ -13,32 +13,32 @@ public class TestMech {
     }
 
     //---------------构造方法-------------------
-    //（默认的构造方法）
+   /*（默认的构造方法）*/
     TestMech(String str){
         System.out.println("(默认)的构造方法 s = " + str);
     }
 
-    //无参构造方法
+    /*无参构造方法*/
     public TestMech(){
         System.out.println("调用了公有、无参构造方法执行了。。。");
     }
 
-    //有一个参数的构造方法
+    /*有一个参数的构造方法*/
     public TestMech(char name){
         System.out.println("姓名：" + name);
     }
 
-    //有多个参数的构造方法
+    /*有多个参数的构造方法*/
     public TestMech(String name ,int age){
         System.out.println("姓名："+name+"年龄："+ age);//这的执行效率有问题，以后解决。
     }
 
-    //受保护的构造方法
+    /*受保护的构造方法*/
     protected TestMech(boolean n){
         System.out.println("受保护的构造方法 n = " + n);
     }
 
-    //私有构造方法
+    /*私有构造方法*/
     private TestMech(int age){
         System.out.println("私有的构造方法   年龄："+ age);
     }
